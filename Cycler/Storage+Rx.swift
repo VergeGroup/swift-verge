eddd2199a4ae734c88dc6e7112fe1fc487c9b6dc
muukii <m@muukii.me>
2018-01-31 01:14:50 +0900

    Add changed

--- Cycler/Storage+Rx.swift
+++ Cycler/Storage+Rx.swift
@@ -50,22 +50,47 @@ extension Storage {
     }
   }
 
+  public func changed<S>(_ target: KeyPath<T, S>, _ comparer: @escaping (S, S) throws -> Bool) -> Observable<S> {
+    return
+      asObservable()
+        .map { $0[keyPath: target] }
+        .distinctUntilChanged(comparer)
+  }
+
+  public func changedDriver<S>(_ target: KeyPath<T, S>, _ comparer: @escaping (S, S) throws -> Bool) -> Driver<S> {
+    return
+      asObservable()
+        .map { $0[keyPath: target] }
+        .distinctUntilChanged(comparer)
+        .asDriver(onErrorRecover: { _ in .empty() })
+  }
+
   public func asObservable() -> Observable<T> {
     return subject.asObservable()
   }
 
+  @available(*, deprecated, message: "Use changed()")
   public func asObservable<S>(keyPath: KeyPath<T, S>) -> Observable<S> {
     return asObservable()
       .map { $0[keyPath: keyPath] }
   }
 
+  @available(*, deprecated)
   public func asDriver() -> Driver<T> {
     return subject.asDriver()
   }
 
+  @available(*, deprecated, message: "Use changedDriver()")
   public func asDriver<S>(keyPath: KeyPath<T, S>) -> Driver<S> {
     return asDriver()
       .map { $0[keyPath: keyPath] }
+  }
+
+  public func map<U>(_ keyPath: KeyPath<T, U>) -> Storage<U> {
+    return
+      map {
+        $0[keyPath: keyPath]
+    }
   }
 
   public func map<U>(_ closure: @escaping (T) -> U) -> Storage<U> {
