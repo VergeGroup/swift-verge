4d0a4c41b4398ab866a5d2626c91b9733ba26a34
Muukii <muukii.app@gmail.com>
2018-06-16 13:26:54 +0900

    Add Target

--- Cycler/Cycler.h
+++ Cycler/Cycler.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 muukii. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for CycleViewModel.
 FOUNDATION_EXPORT double CycleViewModelVersionNumber;
