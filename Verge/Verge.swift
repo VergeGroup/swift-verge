844fa5a7442f0d320948efd2bebf38bb2b03ec61
Muukii <muukii.app@gmail.com>
2019-07-16 21:44:45 +0900

    Introduce Emitter

--- Verge/Verge.swift
+++ Verge/Verge.swift
@@ -47,6 +47,7 @@ public protocol AnyVergeType : class {
 public protocol VergeType : AnyVergeType {
   associatedtype State
   associatedtype Activity
+  var activity: Emitter<Activity> { get }
   var state: Storage<State> { get }
 }
 
@@ -95,8 +96,8 @@ extension VergeType {
     state.mutableStateStorage.loggers = [logger]
   }
 
-  public var activity: Signal<Activity> {
-    return associated.activity.asSignal()
+  public var activity: Emitter<Activity> {
+    return associated.activity
   }
 
   private var lock: NSRecursiveLock {
@@ -248,7 +249,7 @@ extension VergeType {
     line: UInt = #line
     ) {
 
-    associated.activity.accept(activity)
+    associated.activity.makeEmitter().accept(activity)
     logger.didEmit(activity: activity, file: file, function: function, line: line, on: self)
   }
 }
@@ -365,7 +366,7 @@ final class VergeAssociated<Activity> {
 
   var logger: VergeLogging?
 
-  let activity: PublishRelay<Activity> = .init()
+  let activity: Emitter<Activity> = .init()
 
   init() {
 
