a66e6d7b7ffc10742919966e4404b64842ac2410
Muukii <muukii.app@gmail.com>
2019-09-17 11:14:23 +0900

    Add VergeNeue

---
+++ VergeNeue/VergeNeue.h
@@ -0,0 +1,19 @@
+//
+//  VergeNeue.h
+//  VergeNeue
+//
+//  Created by muukii on 2019/09/17.
+//  Copyright © 2019 muukii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VergeNeue.
+FOUNDATION_EXPORT double VergeNeueVersionNumber;
+
+//! Project version string for VergeNeue.
+FOUNDATION_EXPORT const unsigned char VergeNeueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VergeNeue/PublicHeader.h>
+
+
