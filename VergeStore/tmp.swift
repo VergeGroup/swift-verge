4d7ff51296e3ea2301eff2ca21342ad626c9ef0f
Muukii <muukii.app@gmail.com>
2019-11-04 00:35:54 +0900

    :evergreen_tree: Update

---
+++ VergeStore/tmp.swift
@@ -0,0 +1,122 @@
+//
+//  tmp.swift
+//  VergeStore
+//
+//  Created by muukii on 2019/11/04.
+//  Copyright © 2019 muukii. All rights reserved.
+//
+
+import Foundation
+
+public protocol VergeStoreLogger {
+  
+  func willCommit(store: Any, state: Any, mutation: MutationMetadata)
+  func didCommit(store: Any, state: Any, mutation: MutationMetadata)
+  func didDispatch(store: Any, state: Any, action: ActionMetadata)
+}
+
+open class VergeDefaultStore<State> {
+  
+  public var state: State {
+    storage.value
+  }
+  
+  let storage: Storage<State>
+  
+  public private(set) var logger: VergeStoreLogger?
+  
+  public init(
+    initialState: State,
+    logger: VergeStoreLogger?
+  ) {
+    
+    self.storage = .init(initialState)
+    self.logger = logger
+    
+  }
+      
+}
+
+open class Dispatcher<State> {
+  
+  public let targetStore: VergeDefaultStore<State>
+  
+  public init(target store: VergeDefaultStore<State>) {
+    self.targetStore = store
+  }
+  
+  @discardableResult
+  public func dispatch<ReturnType>(
+    _ name: String = "",
+    _ file: StaticString = #file,
+    _ function: StaticString = #function,
+    _ line: UInt = #line,
+    _ action: (DispatchingActionContext<State>) throws -> ReturnType
+  ) rethrows -> ReturnType {
+    
+    let metadata = ActionMetadata(name: name, file: file, function: function, line: line)
+    
+    let context = DispatchingActionContext<State>.init(dispatcher: self)
+    let result = try action(context)
+    targetStore.logger?.didDispatch(store: self, state: targetStore.state, action: metadata)
+    return result
+    
+  }
+  
+  public func commit(
+    _ name: String = "",
+    _ file: StaticString = #file,
+    _ function: StaticString = #function,
+    _ line: UInt = #line,
+    _ mutation: (inout State) throws -> Void
+  ) rethrows {
+    
+    let metadata = MutationMetadata(name: name, file: file, function: function, line: line)
+    
+    targetStore.logger?.willCommit(store: self, state: targetStore.state, mutation: metadata)
+    defer {
+      targetStore.logger?.didCommit(store: self, state: targetStore.state, mutation: metadata)
+    }
+    
+    try targetStore.storage.update { (state) in
+      try mutation(&state)
+    }
+  }
+  
+}
+
+public final class DispatchingActionContext<State> {
+  
+  public let dispatcher: Dispatcher<State>
+  
+  public var state: State {
+    return dispatcher.targetStore.state
+  }
+  
+  init(dispatcher: Dispatcher<State>) {
+    self.dispatcher = dispatcher
+  }
+  
+  @discardableResult
+  public func dispatch<ReturnType>(
+    _ name: String = "",
+    _ file: StaticString = #file,
+    _ function: StaticString = #function,
+    _ line: UInt = #line,
+    _ action: (DispatchingActionContext<State>) -> ReturnType
+  ) -> ReturnType {
+    
+    dispatcher.dispatch(name, file, function, line, action)
+  }
+  
+  public func commit(
+    _ name: String = "",
+    _ file: StaticString = #file,
+    _ function: StaticString = #function,
+    _ line: UInt = #line,
+    _ mutation: (inout State) -> Void
+  ) {
+    
+    dispatcher.commit(name, file, function, line, mutation)
+  }
+}
