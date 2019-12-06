8ec69cefc0266de607f067d3261a3cd9d0579471
Muukii <muukii.app@gmail.com>
2019-12-07 01:38:31 +0900

    :evergreen_tree: Update

---
+++ Sources/VergeNormalizer/VergeNormalizer.h
@@ -0,0 +1,19 @@
+//
+//  VergeNormalizer.h
+//  VergeNormalizer
+//
+//  Created by muukii on 2019/12/07.
+//  Copyright © 2019 muukii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VergeNormalizer.
+FOUNDATION_EXPORT double VergeNormalizerVersionNumber;
+
+//! Project version string for VergeNormalizer.
+FOUNDATION_EXPORT const unsigned char VergeNormalizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VergeNormalizer/PublicHeader.h>
+
+
