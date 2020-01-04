4c8b811e97711e61e00c38b6265ed9419c8bf158
Muukii <muukii.app@gmail.com>
2020-01-04 12:34:16 +0900

    Add experimental framework

---
+++ Sources/VergeGetterCombine/VergeGetterCombine.h
@@ -0,0 +1,19 @@
+//
+//  VergeGetterCombine.h
+//  VergeGetterCombine
+//
+//  Created by muukii on 2020/01/04.
+//  Copyright © 2020 muukii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VergeGetterCombine.
+FOUNDATION_EXPORT double VergeGetterCombineVersionNumber;
+
+//! Project version string for VergeGetterCombine.
+FOUNDATION_EXPORT const unsigned char VergeGetterCombineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VergeGetterCombine/PublicHeader.h>
+
+
