e191261c7c0e822b3afcff768b5cb06d2da7fd2b
Muukii <muukii.app@gmail.com>
2020-01-06 21:36:30 +0900

    signpost

---
+++ Sources/VergeCore/Signpost.swift
@@ -0,0 +1,33 @@
+import os
+
+@available(iOS 12, macOS 10.14, *)
+fileprivate enum Static {
+  static let log = OSLog(subsystem: "me.muukii.Verge", category: .pointsOfInterest)
+}
+
+public struct SignpostTransaction {
+  
+  @usableFromInline
+  let _end: () -> Void
+  
+  public init(_ name: StaticString) {
+    #if DEBUG
+    if #available(iOS 12, macOS 10.14, *) {
+      let id = OSSignpostID(log: Static.log)
+      os_signpost(.begin, log: Static.log, name: name, signpostID: id)
+      _end = {
+        os_signpost(.end, log: Static.log, name: name, signpostID: id)
+      }
+    } else {
+      _end = {}
+    }
+    #else
+    _end = {}
+    #endif
+  }
+    
+  @inlinable
+  public func end() {
+    _end()
+  }
+}
