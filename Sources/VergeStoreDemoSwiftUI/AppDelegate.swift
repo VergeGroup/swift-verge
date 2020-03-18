184cef847c51babdd694b4a80467ca3d2f8a9511
Hiroshi Kimura <muukii.app@gmail.com>
2020-03-19 02:03:40 +0900

    Use unfair-lock in EqualityComputer (#84)

--- Sources/VergeStoreDemoSwiftUI/AppDelegate.swift
+++ Sources/VergeStoreDemoSwiftUI/AppDelegate.swift
@@ -11,10 +11,9 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    
     return true
   }
 
