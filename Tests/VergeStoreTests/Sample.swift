35936130e3eeb621396c060f4ccedcc0e2efe7f5
Muukii <muukii.app@gmail.com>
2020-04-18 03:38:47 +0900

    Update Sample code

---
+++ Tests/VergeStoreTests/Sample.swift
@@ -0,0 +1,57 @@
+//
+//  Sample.swift
+//  VergeStoreTests
+//
+//  Created by muukii on 2020/04/18.
+//  Copyright © 2020 muukii. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+import VergeStore
+
+enum Sample {
+  
+  struct State: StateType {
+    var name: String = ""
+    var age: Int = 0
+  }
+  
+  enum Activity {
+    case somethingHappen
+  }
+  
+  class ViewController: UIViewController {
+    
+    private let nameLabel: UILabel = .init()
+    private let ageLabel: UILabel = .init()
+    
+    let store = Store<State, Activity>(initialState: .init(), logger: nil)
+    
+    var subscriptions = Set<UntilDeinitCancellable>()
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      store.subscribeStateChanges { [weak self] (changes) in
+        self?.update(changes: changes)
+      }
+      .store(in: &subscriptions)
+    
+    }
+    
+    private func update(changes: Changes<State>) {
+      
+      changes.ifChanged(\.name) { (name) in
+        nameLabel.text = name
+      }
+      
+      changes.ifChanged(\.age) { (age) in
+        ageLabel.text = age.description
+      }
+      
+    }
+    
+  }
+}
