aae33db54255b513e20c26395794ef2e42da6561
Muukii <muukii.app@gmail.com>
2020-05-01 23:21:59 +0900

    Add Tests

---
+++ Tests/VergeRxTests/ChangedOperatorTests.swift
@@ -0,0 +1,47 @@
+//
+//  ChangedOperatorTests.swift
+//  VergeRxTests
+//
+//  Created by muukii on 2020/05/01.
+//  Copyright © 2020 muukii. All rights reserved.
+//
+
+
+import Foundation
+
+import XCTest
+
+import VergeRx
+import VergeStore
+import VergeORM
+
+class ChangedOperatorTests: XCTestCase {
+  
+  let store = Store<DemoState, Never>.init(initialState: .init(), logger: nil)
+  
+  func testChanged() {
+    
+    let count = store.derived(.init(map: \.count))
+    
+    let exp = expectation(description: "")
+    exp.assertForOverFulfill = true
+    exp.expectedFulfillmentCount = 3
+    
+    _ = count.rx.changesObservable()
+      .changed({ $0.description })
+      .subscribe(onNext: { _ in
+        exp.fulfill()
+      })
+    
+    store.commit {
+      $0.count += 1
+    }
+    store.commit {
+      $0.count += 1
+    }
+    store.commit { _ in
+     
+    }
+    wait(for: [exp], timeout: 2)
+  }
+}
