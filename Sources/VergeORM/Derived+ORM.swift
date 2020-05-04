4092b45bf84558e19d263ab563c6e736ee22245b
Muukii <muukii.app@gmail.com>
2020-05-05 04:31:19 +0900

    :evergreen_tree: Update

--- Sources/VergeORM/Derived+ORM.swift
+++ Sources/VergeORM/Derived+ORM.swift
@@ -256,21 +256,21 @@ extension StoreType where State : DatabaseEmbedding {
 /// A result instance that contains created Derived object
 /// While creating non-null derived from entity id, some entity may be not founded.
 /// Created derived object are stored in hashed storage to the consumer can check if the entity was not found by the id.
-public struct NonNullDerivedResult<Entity: EntityType> {
+public struct DerivedResult<Entity: EntityType, Derived: AnyObject> {
   
   /// A dictionary of Derived that stored by id
   /// It's faster than filtering values array to use this dictionary to find missing id or created id.
-  public private(set) var storage: [Entity.EntityID : Entity.NonNullDerived] = [:]
+  public private(set) var storage: [Entity.EntityID : Derived] = [:]
   
   /// An array of Derived that orderd by specified the order of id.
-  public private(set) var values: [Entity.NonNullDerived]
+  public private(set) var values: [Derived]
   
-  fileprivate init() {
+  public init() {
     self.storage = [:]
     self.values = []
   }
   
-  fileprivate mutating func append(derived: Entity.NonNullDerived, id: Entity.EntityID) {
+  public mutating func append(derived: Derived, id: Entity.EntityID) {
     storage[id] = derived
     values.append(derived)
   }
@@ -316,6 +316,8 @@ extension StoreType where State : DatabaseEmbedding {
   ) -> Entity.NonNullDerived {
     derivedNonNull(from: entity, dropsOutput: ==)
   }
+  
+  public typealias NonNullDerivedResult<Entity: EntityType> = DerivedResult<Entity, Entity.NonNullDerived>
     
   @inline(__always)
   public func derivedNonNull<Entity: EntityType, S: Sequence>(
