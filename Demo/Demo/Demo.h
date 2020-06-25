2d01df987e384e1681b8129df8df35c9a8c63225
Hiroshi Kimura <muukii.app@gmail.com>
2020-06-26 01:18:56 +0900

    Cleanup structure of demo application (#134)
    * Add Demo
    
    * :evergreen_tree: Update
    
    * :evergreen_tree: Update
    
    * Update cartfile

---
+++ Demo/Demo/Demo.h
@@ -0,0 +1,19 @@
+//
+//  Demo.h
+//  Demo
+//
+//  Created by muukii on 2020/06/26.
+//  Copyright © 2020 muukii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Demo.
+FOUNDATION_EXPORT double DemoVersionNumber;
+
+//! Project version string for Demo.
+FOUNDATION_EXPORT const unsigned char DemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo/PublicHeader.h>
+
+
