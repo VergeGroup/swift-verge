cdba4180615ec92c4c4b2772c9a1c778a3f648ba
Muukii <muukii.app@gmail.com>
2020-06-26 00:41:42 +0900

    :evergreen_tree: Update

---
+++ Demo/VergeStoreDemoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VergeStoreDemoSwiftUI
+//
+//  Created by muukii on 2020/06/26.
+//  Copyright © 2020 muukii. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo/VergeStoreDemoUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VergeStoreDemoUIKit
+//
+//  Created by muukii on 2020/06/26.
+//  Copyright © 2020 muukii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
