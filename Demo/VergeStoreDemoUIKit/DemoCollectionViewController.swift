4b482482bcdc00eb5f10a9ccb43c5a052f93d97b
Muukii <muukii.app@gmail.com>
2020-11-27 09:43:30 +0900

    Update Demo

--- Demo/VergeStoreDemoUIKit/DemoCollectionViewController.swift
+++ Demo/VergeStoreDemoUIKit/DemoCollectionViewController.swift
@@ -2,8 +2,80 @@
 import UIKit
 import VergeStore
 
-final class DemoCollectionViewController: UIViewController {
+final class DemoCollectionViewController: UIViewController, UICollectionViewDelegateFlowLayout {
 
+  @IBOutlet weak var collectionView: UICollectionView!
+
+  private let viewModel = DemoCollectionViewModel()
+  private var subscription: VergeAnyCancellable?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let dataSource = UICollectionViewDiffableDataSource<String, DemoCollectionCellModel>(
+      collectionView: collectionView,
+      cellProvider: { collectionView, indexPath, cellModel in
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DemoCell", for: indexPath) as! DemoCollectionCell
+        cell.attach(cellModel: cellModel)
+        return cell
+      })
+
+    collectionView.dataSource = dataSource
+    collectionView.delegate = self
+
+    subscription = viewModel.sinkState { (state) in
+
+      state.ifChanged(\.displayItems) { items in
+        var snapshot = NSDiffableDataSourceSnapshot<String, DemoCollectionCellModel>()
+
+        snapshot.appendSections(["first"])
+        snapshot.appendItems(items)
+
+        dataSource.apply(snapshot)
+      }
+
+    }
+  }
+
+  @IBAction func onTapLoadButton(_ sender: Any) {
+
+    viewModel.fetchItems {
+
+    }
+
+  }
+
+  @IBAction func onTapReductButton(_ sender: Any) {
+    viewModel.reductAllData()
+  }
+
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+    return CGSize(width: collectionView.bounds.width / 6, height: 50)
+  }
+
+}
+
+final class DemoCollectionCell: UICollectionViewCell {
+
+  private var subscription: VergeAnyCancellable?
+
+  @IBOutlet weak var valueLabel: UILabel!
+
+  func attach(cellModel: DemoCollectionCellModel) {
+
+    subscription?.cancel()
+
+    subscription = cellModel.sinkState { [weak self] state in
+      guard let self = self else { return }
+
+      state.ifChanged(\.text) { text in
+        self.valueLabel.text = text
+      }
+
+    }
+
+  }
 }
 
 final class DemoCollectionViewModel: StoreComponentType, Equatable {
@@ -29,7 +101,7 @@ final class DemoCollectionViewModel: StoreComponentType, Equatable {
 
     func generateDummy() -> [Post] {
       (0..<10).map { _ in
-        Post(name: "")
+        Post(name: .randomEmoji())
       }
     }
 
@@ -46,7 +118,7 @@ final class DemoCollectionViewModel: StoreComponentType, Equatable {
       })
 
       self.commit {
-        $0.displayItems = result
+        $0.displayItems += result
       }
 
       completion()
@@ -55,16 +127,30 @@ final class DemoCollectionViewModel: StoreComponentType, Equatable {
 
   }
 
+  func reductAllData() {
+    state.displayItems.forEach { cellModel in
+      cellModel.reduct()
+    }
+  }
+
 }
 
-final class DemoCollectionCellModel: StoreComponentType, Equatable {
+final class DemoCollectionCellModel: StoreComponentType, Hashable {
 
   static func == (lhs: DemoCollectionCellModel, rhs: DemoCollectionCellModel) -> Bool {
     lhs === rhs
   }
 
+  func hash(into hasher: inout Hasher) {
+    ObjectIdentifier(self).hash(into: &hasher)
+  }
+
   struct State: Equatable {
-    var post: Post
+    var post: Post?
+
+    var text: String {
+      post?.name ?? "❌"
+    }
   }
 
   let store: DefaultStore
@@ -80,11 +166,31 @@ final class DemoCollectionCellModel: StoreComponentType, Equatable {
     }
   }
 
+  func reduct() {
+    commit {
+      $0.post = nil
+    }
+  }
+
 }
 
 struct Post: Hashable {
 
   let id: UUID = UUID()
 
-  var name: String
+  var name: String = .randomEmoji()
+}
+
+extension String{
+  static func randomEmoji() -> String{
+    let range = 0x1F601...0x1F64F
+    let ascii = range.lowerBound + Int(arc4random_uniform(UInt32(range.count)))
+
+    var view = UnicodeScalarView()
+    view.append(UnicodeScalar(ascii)!)
+
+    let emoji = String(view)
+
+    return emoji
+  }
 }
