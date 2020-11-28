30f085ad812e42ee52a8b45af38e28dc7cb6ef04
Muukii <muukii.app@gmail.com>
2020-11-28 23:20:25 +0900

    Update docs

--- Docs/sidebars.js
+++ Docs/sidebars.js
@@ -1,72 +1,72 @@
 module.exports = {
   docs: [{
       type: 'category',
-      label: 'Verge',
+      label: 'Introduction',
       collapsed: false,
-      items: [
-        'installation',
+      items: [    
         'motivation',
         'Overview',
         'demo',
+        'installation',
+        'store/BasicUsage',
+        'store/advanced-usage',
       ]
     },
     {
       type: 'category',
-      label: 'VergeStore',
+      label: 'Core concepts',
       collapsed: false,
       items: [
-        'VergeStore/BasicUsage',
-        'VergeStore/advanced-usage',
+        'store/core/store',          
+        'store/core/state',
+        'store/core/activity',
+        'store/core/extended-computed-property',
+        'store/core/mutation',
+        'store/core/changes',
+        'store/core/derived',
+        'store/core/dispatcher'  
+      ],
+
+    },
+    {
+      type: 'category',
+      label: 'Use in UIKit',
+      collapsed: false,
+      items: [
+        'store/uikit/using-in-uikit',
+        'store/uikit/viewmodel-in-uikit',
+        'store/uikit/using-with-collection-view'
+      ],
+
+    },
+    {
+      type: 'category',
+      label: 'Techniques',
+      collapsed: false,
+      items: [
+        'store/core/logging',
+        'store/techniques/optimization-tips',
+        'store/techniques/utilities'
+      ],
+
+    },
+    'store/migrate-from-classic',
+    {
+      type: 'category',
+      label: 'ORM',
+      collapsed: false,
+      items: [
+        'orm/Overview',
         {
           type: 'category',
           label: 'Docs',
           collapsed: false,
           items: [
-            'VergeStore/store',          
-            'VergeStore/state',
-            'VergeStore/activity',
-            'VergeStore/extended-computed-property',
-            'VergeStore/mutation',
-            'VergeStore/changes',
-            'VergeStore/derived',
-            'VergeStore/dispatcher',
-            'VergeStore/logging',
-            'VergeStore/utilities',
-            'VergeStore/optimization-tips',
+            'orm/index-table',
+            'orm/middleware',
+            'orm/making-derived',
+            'orm/tips',
           ],
-
-        },
-        {
-          type: 'category',
-          label: 'Use in UIKit',
-          collapsed: false,
-          items: [
-            'VergeStore/UIKit/using-in-uikit',
-            'VergeStore/UIKit/viewmodel-in-uikit',
-            'VergeStore/UIKit/using-with-collection-view'
-          ],
-
-        },
-        'VergeStore/migrate-from-classic',
-      ]
-    },
-    {
-      type: 'category',
-      label: 'VergeORM',
-      collapsed: false,
-      items: [
-        'VergeORM/Overview',
-        {
-          type: 'category',
-          label: 'Docs',
-          collapsed: false,
-          items: [
-            'VergeORM/index-table',
-            'VergeORM/middleware',
-            'VergeORM/making-derived',
-            'VergeORM/tips',
-          ],
-
         },
       ]
     }
