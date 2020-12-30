5b4d494881b930ebeb2b08d5c08f91d51436a8c1
Muukii <muukii.app@gmail.com>
2020-12-30 21:52:56 +0900

    :evergreen_tree: Update

--- Docs/docusaurus.config.js
+++ Docs/docusaurus.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: 'Verge - Flux for SwiftUI / UIKit',
   tagline: 'A performant flux library for iOS App - SwiftUI / UIKit',
-  url: 'https://vergegroup.github.io/Verge',
-  baseUrl: '/',
+  url: 'https://vergegroup.github.io/',
+  baseUrl: '/Verge/',
   favicon: 'img/favicon.ico',
   organizationName: 'VergeGroup',
   projectName: 'Verge',
