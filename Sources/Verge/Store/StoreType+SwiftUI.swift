59e20ebfc180573f5c0bb1bf3300f155052e9b5e
Muukii <muukii.app@gmail.com>
2021-01-02 18:28:40 +0900

    Patch

---
+++ Sources/Verge/Store/StoreType+SwiftUI.swift
@@ -0,0 +1,44 @@
+//
+// Copyright (c) 2020 Hiroshi Kimura(Muukii) <muukii.app@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+
+#if canImport(SwiftUI)
+
+import SwiftUI
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+extension BindingDerived {
+
+  /**
+   Still in experimentals.  
+   Converts SwiftUI.Binding from BindingDerived.
+   */
+  public func _swiftUIBinding() -> SwiftUI.Binding<Value> {
+    return .init(get: {
+      self.primitiveValue
+    }, set: { newValue in
+      self.primitiveValue = newValue
+    })
+  }
+}
+
+#endif
