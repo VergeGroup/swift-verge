81833640a04036df8cb615affbf487ada20d1b0b
Muukii <muukii.app@gmail.com>
2021-11-29 12:26:00 +0900

    :zap: WIP

--- Tests/AsyncVergeTests/ConcurrencyFacts.swift
+++ Tests/AsyncVergeTests/ConcurrencyFacts.swift
@@ -50,7 +50,12 @@ final class ConcurrencyFacts: XCTestCase {
       @MainActor
       func entry() async {
 
+        chain3()
+
         _ = await Task.init {
+
+          chain3()
+
           XCTAssertEqual(Thread.current, .main)
 
           await run_async {
@@ -67,22 +72,22 @@ final class ConcurrencyFacts: XCTestCase {
         }
         .result
 
-//        _ = await Task.detached { [self] in
-//          XCTAssertNotEqual(Thread.current, .main)
-//
-//          await run_async {
-//            XCTAssertNotEqual(Thread.current, .main)
-//          }
-//
-//          await run_not_async_mainActor {
-//            XCTAssertEqual(Thread.current, .main)
-//          }
-//
-//          run_not_async_noActor {
-//            XCTAssertNotEqual(Thread.current, .main)
-//          }
-//        }
-//        .result
+        //        _ = await Task.detached { [self] in
+        //          XCTAssertNotEqual(Thread.current, .main)
+        //
+        //          await run_async {
+        //            XCTAssertNotEqual(Thread.current, .main)
+        //          }
+        //
+        //          await run_not_async_mainActor {
+        //            XCTAssertEqual(Thread.current, .main)
+        //          }
+        //
+        //          run_not_async_noActor {
+        //            XCTAssertNotEqual(Thread.current, .main)
+        //          }
+        //        }
+        //        .result
 
         await chain()
       }
@@ -98,6 +103,10 @@ final class ConcurrencyFacts: XCTestCase {
         run_not_async_mainActor {
 
         }
+      }
+
+      @MainActor
+      func chain3() {
       }
 
       func run_async(closure: () -> Void) async {
@@ -119,6 +128,91 @@ final class ConcurrencyFacts: XCTestCase {
     await object.entry()
 
     print("finish")
+
+  }
+
+  @MainActor
+  func test_reentrant() async {
+
+    actor MyActor {
+
+      var values: [String] = []
+
+      func operation(asyncSleep: UInt64, label: String) async {
+
+        values.append("In: \(label)")
+        try! await Task.sleep(nanoseconds: UInt64(asyncSleep) * 1_000_000_000)
+        values.append("Out: \(label)")
+
+      }
+
+    }
+
+    let actor = MyActor()
+
+    await withTaskGroup(of: Void.self) { group in
+
+      group.addTask {
+        await actor.operation(asyncSleep: 3, label: "1")
+      }
+
+      group.addTask {
+        await actor.operation(asyncSleep: 1, label: "2")
+      }
+
+    }
+
+    print(await actor.values)
+
+  }
+
+  @MainActor
+  func test_queueing() async {
+
+    actor MyActor {
+
+      var values: [String] = []
+
+      init() {
+
+      }
+
+      func hoge() -> [String] {
+        values
+      }
+
+      func operation(asyncSleep: UInt64, label: String) async {
+
+        await Task.init {
+          print(values)
+          print("In: \(label)", Thread.current)
+          try! await Task.sleep(nanoseconds: UInt64(asyncSleep) * 1_000_000_000)
+          print("Out: \(label)", Thread.current)
+        }
+        .result
+      }
+
+      private func drain() {
+
+      }
+
+    }
+
+    let actor = MyActor()
+
+    await withTaskGroup(of: Void.self) { group in
+
+      group.addTask {
+        await actor.operation(asyncSleep: 3, label: "1")
+      }
+
+      group.addTask {
+        await actor.operation(asyncSleep: 1, label: "2")
+      }
+
+    }
+
+    print(await actor.values)
 
   }
 
