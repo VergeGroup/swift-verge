74081558dca46e19326f77add1f2794faea5a137
Muukii <muukii.app@gmail.com>
2022-06-13 18:56:13 +0900

    Revert "Clean"
    This reverts commit c771c42f64c60d6dd203e00d36115da1074cf7c7.

---
+++ Sources/VergeRx/VergeRx.h
@@ -0,0 +1,19 @@
+//
+//  VergeRx.h
+//  VergeRx
+//
+//  Created by muukii on 2020/01/09.
+//  Copyright © 2020 muukii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VergeRx.
+FOUNDATION_EXPORT double VergeRxVersionNumber;
+
+//! Project version string for VergeRx.
+FOUNDATION_EXPORT const unsigned char VergeRxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VergeRx/PublicHeader.h>
+
+
