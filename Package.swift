f28929183d310e5237e405509019c62912eccbcd
Muukii <muukii.app@gmail.com>
2022-07-01 01:34:48 +0900

    Update Package description

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
-  name: "Verge",  
+  name: "Verge",
   platforms: [
     .macOS(.v10_12),
     .iOS(.v11),
     .tvOS(.v10),
-    .watchOS(.v3)
+    .watchOS(.v3),
   ],
   products: [
     .library(name: "Verge", targets: ["Verge"]),
@@ -17,7 +17,8 @@ let package = Package(
     .library(name: "VergeClassic", targets: ["VergeClassic"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0")
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
+    .package(url: "https://github.com/apple/swift-docc-plugin.git", branch: "main"),
   ],
   targets: [
     .target(name: "VergeObjcBridge", dependencies: []),
@@ -25,7 +26,15 @@ let package = Package(
     .target(name: "Verge", dependencies: ["VergeObjcBridge"]),
     .target(name: "VergeClassic", dependencies: ["RxSwift", "RxCocoa", "VergeObjcBridge", "VergeRx"]),
     .target(name: "VergeORM", dependencies: ["Verge", "VergeObjcBridge"]),
-    .target(name: "VergeRx", dependencies: ["Verge", "VergeObjcBridge", "RxSwift", "RxCocoa"]),
+    .target(
+      name: "VergeRx",
+      dependencies: [
+        "Verge",
+        "VergeObjcBridge",
+        .product(name: "RxSwift", package: "RxSwift"),
+        .product(name: "RxCocoa", package: "RxSwift")
+      ]
+    ),
   ],
   swiftLanguageVersions: [.v5]
 )
