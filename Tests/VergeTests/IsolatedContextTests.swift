70ccee5805779ec4e23920e4b28fc0260e48c008
Muukii <muukii.app@gmail.com>
2022-08-25 23:32:26 +0900

    :evergreen_tree: Update

--- Tests/VergeTests/IsolatedContextTests.swift
+++ Tests/VergeTests/IsolatedContextTests.swift
@@ -27,9 +27,34 @@ final class IsolatedContextTests: XCTestCase {
     }
     
     _ = store.sinkState(queue: .asyncSerialBackground) { changes in
-      isInMain()
+      Task { @MainActor in
+        isInMain()
+      }
     }
     
+  }
+  
+  func testMainActorSubscription_sink() {
+    
+    // don't add `@MainActor` to make non-isolated-context
+    
+    assert(Thread.isMainThread)
+        
+    let store = DemoStore()
+    
+    var receivedState: DemoStore.State?
+    
+    let sub = store.sinkState { changes in
+      receivedState = changes.primitive
+    }
+    
+    store.commit {
+      $0.count = 100
+    }
+    
+    XCTAssertEqual(receivedState?.count, 100)
+    
+    withExtendedLifetime(sub, {})
   }
   
   
