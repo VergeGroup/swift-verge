7723a77e0e469eabceb1891b43d76a2cd0b5d88b
Muukii <muukii.app@gmail.com>
2022-12-28 00:51:58 +0900

    :zap: WIP

--- Sources/Verge/Library/Log.swift
+++ Sources/Verge/Library/Log.swift
@@ -13,3 +13,39 @@ import os.log
 public enum VergeOSLogs {
   public static let debugLog = OSLog(subsystem: "Verge", category: "Debug")
 }
+
+enum Log {
+  
+  static func debug(
+    file: StaticString = #file,
+    line: UInt = #line,
+    _ log: OSLog,
+    _ object: @autoclosure () -> Any
+  ) {
+    os_log(.default, log: log, "%{public}@\n%{public}@:%{public}@", "\(object())", "\(file)", "\(line.description)")
+  }
+  
+  static func error(
+    file: StaticString = #file,
+    line: UInt = #line,
+    _ log: OSLog,
+    _ object: @autoclosure () -> Any
+  ) {
+    os_log(.error, log: log, "%{public}@\n%{public}@:%{public}@", "\(object())", "\(file)", "\(line.description)")
+  }
+  
+}
+
+extension OSLog {
+  
+  @inline(__always)
+  private static func makeOSLogInDebug(isEnabled: Bool = true, _ factory: () -> OSLog) -> OSLog {
+#if DEBUG
+    return factory()
+#else
+    return .disabled
+#endif
+  }
+  
+  static let storeReader: OSLog = makeOSLogInDebug { OSLog.init(subsystem: "org.VergeGroup", category: "StoreReader") }
+}
