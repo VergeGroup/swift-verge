a4b0f2ad07a358e6157d73eca96950bb187b36e4
Muukii <muukii.app@gmail.com>
2023-10-22 16:56:32 +0900

    Update tests

--- Tests/VergeTests/Isolated/IsolatedStoreTests.swift
+++ Tests/VergeTests/Isolated/IsolatedStoreTests.swift
@@ -2,22 +2,73 @@
 import Verge
 import XCTest
 
-final class IsolatedStoreTests: XCTestCase {
+final class MainActorIsolatedStoreTests: XCTestCase {
 
   @MainActor
   func test_mainActorStore_in_mainActor() {
 
     let store = MainActorStore<_, Never>(initialState: DemoState())
 
+    XCTAssertEqual(store.state.count, 0)
+
     store.commit {
-      $0.count += 1
+      $0.count = 1
     }
 
     XCTAssertEqual(store.state.count, 1)
 
   }
 
-  func test_async() {
+  nonisolated func test_mainActorStore_in_nonisolated() async {
+
+    let store = await MainActorStore<_, Never>(initialState: DemoState())
+
+    do {
+      let count = await store.state.count
+      XCTAssertEqual(count, 0)
+    }
+
+    await store.commit {
+      $0.count = 1
+    }
+
+    do {
+      let count = await store.state.count
+      XCTAssertEqual(count, 1)
+    }
+
+  }
+
+}
+
+final class ActorIsolatedStoreTests: XCTestCase {
+
+  @MainActor
+  func test_asyncStore_in_mainActor() async {
+
+    let store = AsyncStore<_, Never>(initialState: DemoState())
+
+    XCTAssertEqual(store.state.count, 0)
+
+    await store.backgroundCommit {
+      $0.count = 1
+    }
+
+    XCTAssertEqual(store.state.count, 1)
+
+  }
+
+  nonisolated func test_mainActorStore_in_nonisolated() async {
+
+    let store = AsyncStore<_, Never>(initialState: DemoState())
+
+    XCTAssertEqual(store.state.count, 0)
+
+    await store.backgroundCommit {
+      $0.count = 1
+    }
+
+    XCTAssertEqual(store.state.count, 1)
 
   }
 
