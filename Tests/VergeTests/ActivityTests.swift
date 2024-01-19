32e44aad0db6a8e68c4b12b97707fb655aad83b0
Hiroshi Kimura <muukii.app@gmail.com>
2024-01-19 22:14:28 +0900

    Update test (#458)

--- Tests/VergeTests/ActivityTests.swift
+++ Tests/VergeTests/ActivityTests.swift
@@ -44,7 +44,8 @@ class ActivityTests: XCTestCase {
     let store = Store()
     
     let waiter = XCTestExpectation()
-    
+    waiter.expectedFulfillmentCount = 1
+
     store
       .activityPublisher()
       .sink { event in
