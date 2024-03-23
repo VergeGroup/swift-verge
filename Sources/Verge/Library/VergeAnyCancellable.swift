eba4197aab45e10e5562496783abaeb961a580a2
Muukii <muukii.app@gmail.com>
2024-03-23 19:21:31 +0900

    Patch

--- Sources/Verge/Library/VergeAnyCancellable.swift
+++ Sources/Verge/Library/VergeAnyCancellable.swift
@@ -1,5 +1,5 @@
 import Combine
-@_implementationOnly import Atomics
+import Atomics
 
 /// A typealias to `Set<AnyCancellable>`.
 public typealias VergeAnyCancellables = Set<AnyCancellable>
--- Sources/Verge/Store/Store.swift
+++ Sources/Verge/Store/Store.swift
@@ -23,7 +23,7 @@ import Foundation
 import os.log
 import ConcurrencyTaskManager
 
-@_implementationOnly import Atomics
+import Atomics
 
 #if canImport(Combine)
 import Combine
