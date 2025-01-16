90981fef15b3a9dfdefdd28f6a53db82543d515c
Muukii <muukii.app@gmail.com>
2025-01-16 23:41:46 +0900

    Patch

--- Sources/VergeNormalizationDerived/EntityType+Typealias.swift
+++ Sources/VergeNormalizationDerived/EntityType+Typealias.swift
@@ -1,7 +1,21 @@
-
 extension EntityType {
 
   public typealias Derived = Verge.Derived<EntityWrapper<Self>>
   public typealias NonNullDerived = Verge.Derived<NonNullEntityWrapper<Self>>
+
+}
+
+extension Derived where Value: NonNullEntityWrapperType {
+
+  public var entityID: Value.Entity.TypedID {
+    self.state.id
+  }
+}
+
+extension Derived where Value: EntityWrapperType {
+
+  public var entityID: Value.Entity.TypedID? {
+    self.state.id
+  }
 
 }
