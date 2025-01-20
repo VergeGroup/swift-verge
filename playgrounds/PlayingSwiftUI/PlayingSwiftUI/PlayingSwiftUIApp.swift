15d3ce9262d141cd978cf313d511dc81757573b5
Muukii <muukii.app@gmail.com>
2025-01-20 13:53:27 +0900

    Add playgrounds

---
+++ playgrounds/PlayingSwiftUI/PlayingSwiftUI/PlayingSwiftUIApp.swift
@@ -0,0 +1,32 @@
+//
+//  PlayingSwiftUIApp.swift
+//  PlayingSwiftUI
+//
+//  Created by Muukii on 2025/01/20.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PlayingSwiftUIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ playgrounds/PlayingSwiftUI/PlayingSwiftUITests/PlayingSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingSwiftUITests.swift
+//  PlayingSwiftUITests
+//
+//  Created by Muukii on 2025/01/20.
+//
+
+import Testing
+@testable import PlayingSwiftUI
+
+struct PlayingSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
