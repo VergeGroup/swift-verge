20f74e5fd5b55cb35268a896d6c664e380e63cb8
Hiroshi Kimura <muukii.app@gmail.com>
2025-01-21 14:44:52 +0900

    Drop VergeTiny (#504)

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
   ],
   products: [
     .library(name: "Verge", targets: ["Verge"]),
-    .library(name: "VergeTiny", targets: ["VergeTiny"]),
     .library(name: "VergeNormalizationDerived", targets: ["VergeNormalizationDerived"]),
     .library(name: "VergeRx", targets: ["VergeRx"]),
     .library(name: "VergeClassic", targets: ["VergeClassic"]),
@@ -46,7 +45,6 @@ let package = Package(
     // macro exports
     .target(name: "VergeMacros", dependencies: ["VergeMacrosPlugin"]),
 
-    .target(name: "VergeTiny", dependencies: []),
     .target(
       name: "Verge",
       dependencies: [
@@ -97,10 +95,6 @@ let package = Package(
       swiftSettings: [
         .enableExperimentalFeature("StrictConcurrency")
       ]
-    ),
-    .testTarget(
-      name: "VergeTinyTests",
-      dependencies: ["VergeTiny"]
     ),
     .testTarget(
       name: "VergeMacrosTests",
