3e16bf95e3f3981a9862e60e5345887ffa1d27ba
Muukii <muukii.app@gmail.com>
2025-03-22 12:56:21 +0900

    Update

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     .package(url: "https://github.com/VergeGroup/TypedIdentifier", from: "2.0.2"),
     .package(url: "https://github.com/VergeGroup/TypedComparator", from: "1.0.0"),
     .package(url: "https://github.com/VergeGroup/Normalization", from: "1.1.0"),
-    .package(url: "https://github.com/VergeGroup/swift-macro-state-struct", branch: "muukii/escaping-tracking"),
+    .package(url: "https://github.com/VergeGroup/swift-macro-state-struct", from: "2.0.0"),
 
     /// for testing
     .package(url: "https://github.com/nalexn/ViewInspector.git", from: "0.10.0"),
